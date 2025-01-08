907f3ca74690fa538bf85801ae98c9112ee17e22
acef304 <not@yet.com>
2025-01-08 00:30:26 +0000

    Finally, shortener

--- src/main/scala/Shortener.scala
+++ src/main/scala/Shortener.scala
@@ -1,39 +1,61 @@
 import org.scalatest.funsuite.AsyncFunSuite
 
-import java.util.concurrent.atomic.AtomicReference
-import scala.collection.mutable.HashMap
+import scala.collection.mutable
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
-import scala.collection.concurrent.TrieMap
+
 
 class Shortener {
-  val shortToFull = Map.empty[String, String]
-  val fullToShort = Map.empty[String, String]
-  case class State(shortToFull: TrieMap[String, String], fullToShort: TrieMap[String, String])
-  val state = new AtomicReference(State(TrieMap.empty, TrieMap.empty))
+  val shortToFull = mutable.HashMap.empty[String, String]
+  val fullToShort = mutable.HashMap.empty[String, String]
 
-  state.compareAndExchange()
-  def getShortLink(fullLink: String): String =
+  def getShortLink(fullLink: String): String = synchronized {
     if (fullToShort.contains(fullLink))
       fullToShort(fullLink)
     else {
       val shortLink = Random.alphanumeric.take(5).mkString
-      fullToShort.(fullLink, shortLink)
+      fullToShort.put(fullLink, shortLink)
       shortToFull.put(shortLink, fullLink)
       shortLink
     }
+  }
+
   def getFullLink(shortLink: String): Option[String] = shortToFull.get(shortLink)
 }
 
 
 import org.scalatest.funsuite.AsyncFunSuite
 
-
 class ShortenerTest extends AsyncFunSuite {
-  test("Greet should return string that contains name") {
+  implicit override def executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+  val possibleLinks = (1 to 1000).map("https://example.com/link_" + _)
+
+  test("Full link of a created short link should be equals to original link") {
     val example = "https://example.com"
     val shortener = new Shortener()
     assert(
       shortener.getFullLink(shortener.getShortLink(example)).contains(example)
     )
   }
+
+  test("Short link should be created only once") {
+    val shortener = new Shortener
+
+    val futuresList = (1 to 10000).map {
+      _ =>
+      Future {
+        val processedLinks = possibleLinks
+          .map(shortener.getShortLink)
+          .flatMap(shortener.getFullLink(_).toList)
+        possibleLinks == processedLinks
+      }
+    }
+
+    Future
+      .sequence(futuresList)
+      .map(checks => assert(!checks.contains(false)))
+
+  }
+
 }
